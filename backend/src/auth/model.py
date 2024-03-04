1f4f9ba6a2b1f8b2041e260cf5d5c34fb7c79967
Marcin Niemczyk <m_niemczyk@outlook.com>
2024-03-04 19:41:29 +0100

    feat(api): add user model

---
+++ backend/src/auth/model.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.database.engine import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str] = mapped_column(unique=True)
+    password: Mapped[str]
