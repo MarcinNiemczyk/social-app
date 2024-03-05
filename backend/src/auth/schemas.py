91dbb7882b21e43c3c5d6742727177de62e91d72
Marcin Niemczyk <m_niemczyk@outlook.com>
2024-03-05 20:09:12 +0100

    feat(api): add user schemas

---
+++ backend/src/auth/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel, EmailStr
+
+
+class UserRegister(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserRead(BaseModel):
+    email: EmailStr
