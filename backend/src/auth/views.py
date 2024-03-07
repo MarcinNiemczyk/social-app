6f5fe971367c5a4b10acf526992cc8d3bfd68baa
Marcin Niemczyk <m_niemczyk@outlook.com>
2024-03-07 20:25:19 +0100

    feat(api): add login endpoint

--- backend/src/auth/views.py
+++ backend/src/auth/views.py
@@ -3,7 +3,7 @@ from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 
 from src.auth.model import User
-from src.auth.schemas import UserRegister
+from src.auth.schemas import UserLogin, UserRegister
 from src.database.engine import get_db
 
 auth_router = APIRouter()
@@ -23,3 +23,14 @@ def register(user_in: UserRegister, db_session: Session = Depends(get_db)):
     user = User(email=user_in.email, password=hashed_password)
     db_session.add(user)
     db_session.commit()
+
+
+@auth_router.post("/login")
+def login(user_in: UserLogin, db_session: Session = Depends(get_db)):
+    user = db_session.query(User).filter(User.email == user_in.email).one_or_none()
+    if not user or not pwd_context.verify(user_in.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+        )
+    return {"status": "Logged in successfully"}
