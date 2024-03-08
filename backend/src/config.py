322f7ddc32ad9a9e3e1d3f611aeb457f08f56702
Marcin Niemczyk <m_niemczyk@outlook.com>
2024-03-08 20:05:22 +0100

    feat(api): provide env config template

---
+++ backend/src/config.py
@@ -0,0 +1,20 @@
+import logging
+
+from passlib.context import CryptContext
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+log = logging.getLogger(__name__)
+
+
+class Config(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
+
+config = Config()
+if not config.SECRET_KEY:
+    log.warning("No JWT Secret specified. Authentication system is suppressed.")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
