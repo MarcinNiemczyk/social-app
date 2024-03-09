03e0994e86dc92d313b5aa8e3ff565e3b88f068d
Marcin Niemczyk <m_niemczyk@outlook.com>
2024-03-09 09:07:32 +0100

    fix(api): fix reading env in config file

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,3 +1,4 @@
+import os
 import logging
 
 from passlib.context import CryptContext
@@ -5,9 +6,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 log = logging.getLogger(__name__)
 
+DOTENV = os.path.join(os.path.dirname(__file__), ".env")
 
 class Config(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=DOTENV, env_file_encoding="utf-8")
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
