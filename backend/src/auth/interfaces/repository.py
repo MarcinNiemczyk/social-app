fe4e13029cc8e99c6bb250dc92be9c579e50ad87
Marcin Niemczyk <m_niemczyk@outlook.com>
2024-03-13 19:23:46 +0100

    refactor(api): refactor type hints in user repo interface

--- backend/src/auth/interfaces/repository.py
+++ backend/src/auth/interfaces/repository.py
@@ -1,16 +1,21 @@
 from abc import ABC, abstractmethod
+from typing import TYPE_CHECKING, Optional, TypeVar
 
 from sqlalchemy.orm import Session
 
-from src.auth.model import User
 from src.auth.schemas.model_schema import UserCreate
+
+if TYPE_CHECKING:
+    from src.auth.model import User
+
+    UserType = TypeVar("UserType", bound=User)
 
 
 class IAuthRepository(ABC):
     @abstractmethod
-    def create(self, db: Session, user: UserCreate) -> None:
+    def create(self, db: Session, user_in: UserCreate) -> None:
         raise NotImplementedError
 
     @abstractmethod
-    def get_by_email(self, db: Session, email: str) -> User:
+    def get_by_email(self, db: Session, email: str) -> Optional[UserType]:
         raise NotImplementedError
