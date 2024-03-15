7057d4c4cd2ef4601a3050f55c02f598a9b515bd
Marcin Niemczyk <m_niemczyk@outlook.com>
2024-03-15 18:39:09 +0100

    refactor(api): cleanup config file

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,12 +1,12 @@
-import os
 import logging
+import os
 
-from passlib.context import CryptContext
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 log = logging.getLogger(__name__)
 
 DOTENV = os.path.join(os.path.dirname(__file__), ".env")
+
 
 class Config(BaseSettings):
     model_config = SettingsConfigDict(env_file=DOTENV, env_file_encoding="utf-8")
@@ -15,8 +15,6 @@ class Config(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int
 
 
-config = Config()
+config = Config()  # type: ignore
 if not config.SECRET_KEY:
     log.warning("No JWT Secret specified. Authentication system is suppressed.")
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
