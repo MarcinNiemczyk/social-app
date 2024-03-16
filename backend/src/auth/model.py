5737ccb4773d307c00fa54b5612ae8d2de86ef19
Marcin Niemczyk <m_niemczyk@outlook.com>
2024-03-16 13:08:30 +0100

    chore(api)!: replace user id with uuid

--- backend/src/auth/model.py
+++ backend/src/auth/model.py
@@ -1,3 +1,5 @@
+from uuid import UUID, uuid4
+
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database.engine import Base
@@ -6,6 +8,6 @@ from src.database.engine import Base
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
     email: Mapped[str] = mapped_column(unique=True)
     password: Mapped[bytes]
