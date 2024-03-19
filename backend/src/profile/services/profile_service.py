af3685a914b280be78ac5cdaefa4b194142309b6
Marcin Niemczyk <m_niemczyk@outlook.com>
2024-03-19 20:34:41 +0100

    refactor(api): add and utilize profile service

---
+++ backend/src/profile/services/profile_service.py
@@ -0,0 +1,31 @@
+from uuid import UUID
+
+from sqlalchemy.orm import Session
+
+from src.profile.interfaces.repository import IProfileRepository
+from src.profile.schemas.endpoint_schema import (
+    ProfileCreatePayload,
+    ProfileRead,
+    ProfileUpdatePayload,
+)
+from src.profile.schemas.model_schema import ProfileCreate, ProfileUpdate
+
+
+class ProfileService:
+    def __init__(self, repository: IProfileRepository):
+        self.repository = repository
+
+    def create_profile(self, db: Session, profile_in: ProfileCreatePayload) -> None:
+        self.repository.create(db, ProfileCreate(**profile_in.model_dump()))
+
+    def list_profiles(self, db: Session) -> list[ProfileRead]:
+        profiles = self.repository.get_all(db)
+        return [ProfileRead(**profile.__dict__) for profile in profiles]
+
+    def update_profile(self, db: Session, profile_id: UUID, profile_in: ProfileUpdatePayload) -> None:
+        profile = self.repository.get_by_id(db, profile_id)
+        self.repository.update(db, ProfileUpdate(**profile_in.model_dump()), profile)
+
+    def get_profile(self, db: Session, profile_id: UUID) -> ProfileRead:
+        profile = self.repository.get_by_id(db, profile_id)
+        return ProfileRead(**profile.__dict__)
--- backend/src/profile/views.py
+++ backend/src/profile/views.py
@@ -1,46 +1,36 @@
 from uuid import UUID
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from src.database.engine import get_db
-from src.profile.model import Profile
+from src.profile.repositories.profile_repository import ProfileRepository
 from src.profile.schemas.endpoint_schema import (
     ProfileCreatePayload,
-    ProfileRead,
     ProfileUpdatePayload,
 )
+from src.profile.services.profile_service import ProfileService
 
 profile_router = APIRouter()
+profile_repository = ProfileRepository()
+profile_service = ProfileService(profile_repository)
 
 
 @profile_router.post("/", status_code=status.HTTP_201_CREATED)
 def create_profile(profile_in: ProfileCreatePayload, db: Session = Depends(get_db)):
-    profile = Profile(**profile_in.model_dump())
-    db.add(profile)
-    db.commit()
+    return profile_service.create_profile(db, profile_in)
 
 
-@profile_router.get("/", response_model=list[ProfileRead])
+@profile_router.get("/")
 def list_profiles(db: Session = Depends(get_db)):
-    return db.query(Profile).all()
+    return profile_service.list_profiles(db)
 
 
 @profile_router.put("/{id}", status_code=status.HTTP_200_OK)
 def update_profile(profile_id: UUID, profile_in: ProfileUpdatePayload, db: Session = Depends(get_db)):
-    profile = db.query(Profile).get(profile_id)
-    if not profile:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Profile not found")
-
-    for key, value in profile_in.model_dump().items():
-        setattr(profile, key, value)
-
-    db.commit()
+    return profile_service.update_profile(db, profile_id, profile_in)
 
 
-@profile_router.get("/{id}", response_model=ProfileRead)
+@profile_router.get("/{id}")
 def get_profile(profile_id: UUID, db: Session = Depends(get_db)):
-    profile = db.get(Profile, profile_id)
-    if not profile:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Profile not found")
-    return profile
+    return profile_service.get_profile(db, profile_id)
