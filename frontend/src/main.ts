97759be57a1bb678c85552444fe88b86ddf81e34
Marcin Niemczyk <m_niemczyk@outlook.com>
2024-03-23 08:58:34 +0100

    build(frontend): mount vuetify to app

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,9 +3,18 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
