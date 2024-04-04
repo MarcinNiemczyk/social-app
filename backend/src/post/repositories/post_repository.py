c1a9ecf06a94738da3c1c09ee79d77127530228e
MarcinNiemczyk <m_niemczyk@outlook.com>
2024-04-04 19:50:44 +0200

    refactor(api): implement post repository

---
+++ backend/src/post/repositories/post_repository.py
@@ -0,0 +1,26 @@
+from typing import Type
+from uuid import UUID
+
+from sqlalchemy.orm import Session
+
+from src.post.interfaces.repository import IPostRepository
+from src.post.schemas.model_schema import PostCreate, PostUpdate
+from src.post.model import Post
+
+
+class PostRepository(IPostRepository):
+    def create(self, db: Session, post_in: PostCreate) -> None:
+        post = Post(**post_in.model_dump())
+        db.add(post)
+        db.commit()
+
+    def get_all(self, db: Session) -> list[Type[Post]]:
+        return db.query(Post).all()
+
+    def update(self, db: Session, post_in: PostUpdate, post: Post) -> None:
+        for key, value in post_in.model_dump().items():
+            setattr(post, key, value)
+        db.commit()
+
+    def get_by_id(self, db: Session, post_id: UUID) -> Post | None:
+        return db.query(Post).get(post_id)
