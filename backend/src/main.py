3a4ab4af70a2008905d5bab5b98cebef52ef6a6a
MarcinNiemczyk <m_niemczyk@outlook.com>
2024-04-10 19:12:37 +0200

    feat(api): include posts router

--- backend/src/main.py
+++ backend/src/main.py
@@ -3,11 +3,13 @@ from fastapi import FastAPI
 
 from src.auth.views import auth_router
 from src.database.engine import Base, engine
+from src.post.views import post_router
 from src.profile.views import profile_router
 
 app = FastAPI()
 app.include_router(auth_router, tags=["Authentication"])
 app.include_router(profile_router, prefix="/profiles", tags=["Profiles"])
+app.include_router(post_router, prefix="/posts", tags=["Posts"])
 
 Base.metadata.create_all(bind=engine)
 
