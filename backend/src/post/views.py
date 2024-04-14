db25a80f4321f682a9b1396d1d7e4225866548e9
MarcinNiemczyk <m_niemczyk@outlook.com>
2024-04-14 19:33:13 +0200

    fix(api): fix posts routes

--- backend/src/post/views.py
+++ backend/src/post/views.py
@@ -16,21 +16,21 @@ post_router = APIRouter()
 post_service = PostService(post_repository)
 
 
-@post_router.get("/posts")
+@post_router.get("/")
 def get_posts(db: Session = Depends(get_db)) -> list[PostRead]:
     return post_service.list_posts(db)
 
 
-@post_router.post("/posts", status_code=status.HTTP_201_CREATED)
+@post_router.post("/", status_code=status.HTTP_201_CREATED)
 def create_post(post_in: PostCreatePayload, db: Session = Depends(get_db)) -> None:
     return post_service.create_post(db, post_in)
 
 
-@post_router.get("/posts/{post_id}")
+@post_router.get("/{post_id}")
 def get_post(post_id: UUID, db: Session = Depends(get_db)) -> PostRead:
     return post_service.get_post(db, post_id)
 
 
-@post_router.put("/posts/{post_id}", status_code=status.HTTP_200_OK)
+@post_router.put("/{post_id}", status_code=status.HTTP_200_OK)
 def update_post(post_id: UUID, post_in: PostUpdatePayload, db: Session = Depends(get_db)) -> None:
     return post_service.update_post(db, post_id, post_in)
